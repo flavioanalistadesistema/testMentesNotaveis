86ae5b61c3ea2729e8ab9281e97f364e56326c12
Flavio Santos <flavioanalistadesistema@gmail.com>
2021-05-10 23:29:20 -0300

    Created e config all models

---
+++ app/Models/Cities.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cities extends Model
+{
+    use HasFactory;
+
+    /**
+    * The table associated with the model.
+    *
+    * @var string
+    */
+   protected $table = 'cities';
+
+   /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
+   protected $fillable = ['users_id', 'city', 'district'];
+}
